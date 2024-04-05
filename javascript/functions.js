6be9184e0ed5f22de59e1570fa51bbbb082a790d
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-04 17:00:29 -0700

    Created a called a basic function.

---
+++ javascript/functions.js
@@ -0,0 +1,10 @@
+//-------------------------------------------------------------------------FUNCTION DECLARATIONS
+
+// Standard function declaration
+function funcInfo(x) {
+  const info = `Parameter = x\nArgument = "${x}"\nIdentifier = funcInfo`;
+  console.log(info);
+}
+
+//-------------------------------------------------------------------------FUNCTION CALLS
+funcInfo("Arg")
