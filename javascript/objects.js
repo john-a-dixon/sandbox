fb5bc24c4038f16946381a74a1cc90cdab710318
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-11 16:30:24 -0700

    Demonstrated iterting through objects.

--- javascript/objects.js
+++ javascript/objects.js
@@ -10,10 +10,15 @@ story["title"] = "Sojourner's Odyssey";
 // Nested objects
 
 const acts = {
-  sequences: [1, 2, 3, 4, 5]
+  sequences: [1, 2, 3, 4, 5],
+  settings: ["A", "B", "C"]
 }
 
 story["acts"] = acts
 
 console.log(story);
 
+// Iterating through objects
+for(let sequence in story.acts) {
+  console.log(story["acts"][sequence]);
+}
