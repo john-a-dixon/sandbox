13d432ae22ce2c716630d06623d35bdab8530dfe
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-11 15:41:27 -0700

    Demonstrated iterated through a for-loop.

--- javascript/loops.js
+++ javascript/loops.js
@@ -3,3 +3,8 @@ for(let i = 0; i < 10; i++) {
   console.log(i + 1);
 }
 
+// Looping through arrays
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for(let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
