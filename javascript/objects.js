0df337d9129fa6d70bafe14aa392f06282b9523d
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-12 16:04:47 -0700

    Demonstrated getters, setters, and this.

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,11 +1,18 @@
 // Basic instance of an object
 const story = {
   content: {
-    _acts: [],
+    _acts: [9],
     _parts: [],
-
+    get acts() {
+      return this._acts;
+    },
+    get parts() {
+      return this._parts;
+    }
   },
   structure: {
     _characters: []
   }
 };
+
+console.log(story.content.acts);
