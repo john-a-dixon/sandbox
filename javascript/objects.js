3ef56253cbd18a49e5b39c48355d4a5825a02538
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-12 16:21:12 -0700

    Reconverted back to standard object. Will make a new object factory.

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,30 +1,27 @@
 // Basic instance of an object
-const story = () => {
-  return {
-    _title: "Untitled",
-    content: {
-      _acts: [9],
-      _parts: [],
-      get acts() {
-        return this._acts;
-      },
-      get parts() {
-        return this._parts;
-      }
+const story = {
+  _title: "Untitled",
+  content: {
+    _acts: [9],
+    _parts: [],
+    get acts() {
+      return this._acts;
     },
-    structure: {
-      _characters: []
-    },
-  
-    set title(title) {
-      this._title = title
-    },
-    get title() {
-      return this._title
+    get parts() {
+      return this._parts;
     }
+  },
+  structure: {
+    _characters: []
+  },
+
+  set title(x) {
+    this._title = x
+  },
+  get title() {
+    return this._title
   }
 };
-
 
 console.log(story.title);
 story.title = "Sojourner's Odyssey";
