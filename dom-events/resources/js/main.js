3d8c23d0807be52a8c72bf7d32d79efa24111b6a
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-16 19:38:54 -0700

    Started the creation of the random color generator.

--- dom-events/resources/js/main.js
+++ dom-events/resources/js/main.js
@@ -3,8 +3,6 @@ const switcher = document.getElementById("switch");
 const colorBox = document.getElementById("color");
 
 // Event handlers
-
-
 const showRGB = event => {
   const color = getComputedStyle(event.target).backgroundColor;
   event.target.innerHTML = color;
@@ -14,6 +12,12 @@ const hideRGB = event => {
   event.target.innerHTML = "";
 };
 
+const randomNumber = number => Math.round(Math.random() * number)
+const changeColor = event => {
+  console.log(randomNumber(255));
+};
+
 // Event listeners
 colorBox.addEventListener("mouseenter", showRGB);
 colorBox.addEventListener("mouseleave", hideRGB);
+switcher.addEventListener("click", changeColor);
