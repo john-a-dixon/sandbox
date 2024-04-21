50debb1406b462dd619e2e1e91facc990d6d6c73
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-21 15:15:42 -0700

    Demonstrated the try-catch block.

--- javascript/error-handling.js
+++ javascript/error-handling.js
@@ -1,10 +1,13 @@
 const err1 = new Error("I am an error");
 const err2 = Error("I am also an error");
 
+try {
+  throw err1;
+} catch(error) {
+  console.log(error);
+  console.log("In the catch.")
+}
 
-throw err1;
-
-
-
-console.log(err1);
 console.log(err2);
+
+console.log("Done");
