b297083c80c8b84abb5b00aeff9ee2cc4cf0a0c1
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-22 17:24:57 -0700

    Done with doing the general formatting for the test.

--- javascript/mocha-tests/tests.js
+++ javascript/mocha-tests/tests.js
@@ -1,1 +1,10 @@
+const assert = require("assert");
 const { altCase } = require("./app.js");
+
+describe("Casing", () => {
+  describe(".altCase", () => {
+    it("returns alternating casing for a word", () => {
+
+    });
+  });
+});
