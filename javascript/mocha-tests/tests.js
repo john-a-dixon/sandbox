3995499cad838cea5835784f6bb6507133bff1e0
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-22 17:31:41 -0700

    Demonstrated some tests.

--- javascript/mocha-tests/tests.js
+++ javascript/mocha-tests/tests.js
@@ -1,10 +1,20 @@
 const assert = require("assert");
 const { altCase } = require("./app.js");
+const exp = require("constants");
 
 describe("Casing", () => {
   describe(".altCase", () => {
     it("returns alternating casing for a word", () => {
+      // Setup
+      const expected = "HeLlO";
+      let actual = "";
 
+      // Exercise
+      actual = altCase("Hello");
+
+      // Verify
+      assert.strictEqual(expected, actual);
+      
     });
   });
 });
