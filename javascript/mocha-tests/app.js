f8863cadd246a42435b2e5cf250e2e1b05e144ef
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-22 17:14:31 -0700

    Added another example of a function.

--- javascript/mocha-tests/app.js
+++ javascript/mocha-tests/app.js
@@ -1,5 +1,18 @@
 const allCaps = phrase => phrase.toUpperCase();
 
 const altCase = phrase => {
-  
-}
+  let altString = ""
+  let capIt = true;
+  for(let character of phrase) {
+    if(capIt) {
+      altString += character.toUpperCase();
+    } else {
+      altString += character.toLowerCase();
+    }
+    capIt = !capIt;
+  }
+
+  return altString;
+};
+
+console.log(altCase("LOL"));
