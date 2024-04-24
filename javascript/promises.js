3e12366dccba5e568fe935a1fa076ab7485fb23b
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-24 16:30:00 -0700

    Done with the mockWait function. No need for testing.

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,8 +1,9 @@
 // Function to demonstrate promises
 const mockWait = (message, time) => {
-
+  setTimeout(() => console.log(message), time);
 }
 
+mockWait("Test", 1000)
 // Standard promise
 
 // Chained promise
