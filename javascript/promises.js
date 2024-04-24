527dff2155f0a6bd1927a70e6f5fb961858353da
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-24 16:52:07 -0700

    Demonstrated a basic promise.

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,10 +1,24 @@
 // Function to demonstrate promises
-const mockWait = (message, time) => {
-  setTimeout(() => console.log(message), time);
-}
+const mockCall = (message, time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randNum = Math.round(Math.random() * 1000);
+      if (randNum % 2) {
+        resolve(message + ": resolved")
+      } else {
+        resolve(message + ": rejected")
+      }
+    }, time);
+  });
+};
 
-mockWait("Test", 1000)
-// Standard promise
+
+// Success & Failure Handlers
+const onResolve = resolved => console.log(resolved);
+const onReject = rejected => console.log(rejected);
+
+// Basic promise
+mockCall("Basic Promise", 200).then(onResolve, onReject);
 
 // Chained promise
 
