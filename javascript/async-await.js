1a7bff390fcb54798897153672d0c863ba6743cc
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-25 15:58:55 -0700

    Now I get it.

--- javascript/async-await.js
+++ javascript/async-await.js
@@ -2,26 +2,18 @@
 // the native-promises.js file (also in comment below) but 
 // using async-await.
 
-// const mockCall = (message, time) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const randNum = Math.round(Math.random() * 1000);
-//       if (randNum % 2) {
-//         resolve(message + ": resolved")
-//       } else {
-//         reject(message + ": rejected")
-//       }
-//     }, time);
-//   });
-// };
-
-const mockCall = async (message, time) => {
-  let result = "";
-  await setTimeout(() => {
-    result = "Resolved";
-  }, time);
-  console.log(result);
-  return result;
+const mockCall = (message, time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randNum = Math.round(Math.random() * 1000);
+      if (randNum % 2) {
+        resolve(message + ": resolved")
+      } else {
+        reject(message + ": rejected")
+      }
+    }, time);
+  });
 };
 
-mockCall("Hello", 1000);
+
+// Basic async-await
