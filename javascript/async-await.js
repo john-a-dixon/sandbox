fb37e43c4d5bf2ba0b32f4c099ef75f3676258c4
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-25 06:06:26 -0700

    Added the previous mockCall function for comparison.

---
+++ javascript/async-await.js
@@ -0,0 +1,17 @@
+// This function uses the same functionality as the one in
+// the native-promises.js file (also in comment below) but 
+// using async-await.
+
+// const mockCall = (message, time) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const randNum = Math.round(Math.random() * 1000);
+//       if (randNum % 2) {
+//         resolve(message + ": resolved")
+//       } else {
+//         reject(message + ": rejected")
+//       }
+//     }, time);
+//   });
+// };
+
