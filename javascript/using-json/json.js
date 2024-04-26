98406329b23d9f31d13b6b91d9800fdb55563f67
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-26 16:18:56 -0700

    Outputted the JSON object.

--- javascript/using-json/json.js
+++ javascript/using-json/json.js
@@ -1,3 +1,4 @@
 const mockJSON = "{ \"book\": { \"name\": \"JSON Primer\", \"price\": 29.99, \"inStock\": true, \"rating\": null } }"
 
 const jsonObject = JSON.parse(mockJSON);
+console.log(jsonObject);
