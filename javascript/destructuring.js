574afbc773bc37dc939a2447690684c6840906cc
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-28 09:01:31 -0700

    Basic destructuring.

--- javascript/destructuring.js
+++ javascript/destructuring.js
@@ -3,6 +3,9 @@ let alpha = ["a", "b", "c"];
 let [a, b, c] = alpha
 console.log(a, b, c);
 
+const [first] = alpha;
+console.log(first);
+
 // Destructuring with objects
 
 // Destructuring with functions
