8a59fe10e7a402902a10c8411f874a327017562c
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-29 05:48:12 -0700

    Demonstrated the fact that self-closing tags must have a forward slash.

--- react/basic/app.js
+++ react/basic/app.js
@@ -1,5 +1,6 @@
 const myElement = (
-  <div id="myElement" className="myClass">
-    <p>An element!</p>
+  <div id="myElement" className="myClass"> {/*Use className to add classes to JSX*/}
+    <p>An element!</p> 
+    <br /> {/*All self-closing tags must have a forward slash.*/}
   </div>
 );
