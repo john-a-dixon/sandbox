cc8059bf35f99f828d08e2cfe863c5b7ef44b29a
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-29 05:56:35 -0700

    Demonstrated the use of the && operator.

--- react/basic/app.js
+++ react/basic/app.js
@@ -6,6 +6,7 @@ const myElement = (
     <br /> {/* All self-closing tags must have a forward slash. */}
     <p>{9 + 1}</p> {/* All injected JS must be in curly braces. */}
     <p>{true ? "This is true" : "This is false"}</p> {/* Ternary operations can be used in injected JS but not if statements. */}
+    {true && <p>"This is another option."</p>} {/* Can use this as well in place of ternary operations. */}
     <button onClick={() => console.log("Click")}></button> {/* All events listeners are camel case. */}
   </div>
 );
