25810ea2c1b5e7ec3d673d31571ab4432dc25788
John Andrew Dixon <john-a-dixon@outlook.com>
2024-04-29 05:50:22 -0700

    Demonstrated injecting real js into a JSX element.

--- react/basic/app.js
+++ react/basic/app.js
@@ -1,6 +1,7 @@
 const myElement = (
-  <div id="myElement" className="myClass"> {/*Use className to add classes to JSX*/}
+  <div id="myElement" className="myClass"> {/* Use className to add classes to JSX. */}
     <p>An element!</p> 
-    <br /> {/*All self-closing tags must have a forward slash.*/}
+    <br /> {/* All self-closing tags must have a forward slash. */}
+    <p>{9 + 1}</p> {/* All injected JS must be in curly braces. */}
   </div>
 );
